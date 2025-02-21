a9ccc23bf5a640050f0bb9dbe0f464c8238aa391
emil19ro <emil19ro@gmail.com>
2025-02-21 16:12:46 +0100

    refactor to use 'btn-cyan-200' style instead of 'btn btn-cyan-200' also active intellisense of tailwind

--- plugins/buttonPlugin.js
+++ plugins/buttonPlugin.js
@@ -1,39 +1,54 @@
 const plugin = require("tailwindcss/plugin");
-const {default: lightOrDarkColor} = require("@check-light-or-dark/color");
+const { default: lightOrDarkColor } = require("@check-light-or-dark/color");
 
-const buttonPlugin = plugin(function({addComponents, matchComponents, theme}){
-  addComponents({
-    ".btn": {
-      display: "inline-block",
-      cursor: "pointer",
-      fontWeight: "bold",
-      padding: `${theme("spacing.2")} ${theme("spacing.4")}`,
-      borderRadius: theme("borderRadius").lg,
-    },
-  });
+const buttonPlugin = plugin(function ({ addComponents, matchUtilities, theme }) {
+  let components = {};
 
-  for(let key in theme("colors")){
-    if(typeof theme("colors")[key] !== "string"){
-      for(let shade in theme("colors")[key]){
-        const colorType = lightOrDarkColor(theme("colors")[key][shade]);
-        addComponents({
-          [`.btn-${key}-${shade}`]: {
-            backgroundColor: theme("colors")[key][shade],
-            color: colorType === "dark" ? "white" : "black",
-          },
-        });
+  components[".btn"] = {
+    display: "inline-block",
+    cursor: "pointer",
+    fontWeight: "bold",
+    padding: `${theme("spacing.2")} ${theme("spacing.4")}`,
+    borderRadius: theme("borderRadius.lg"),
+    transition: "background-color 0.3s, color 0.3s",
+    textAlign: "center",
+  };
+
+  for (let key in theme("colors")) {
+    if (typeof theme("colors")[key] !== "string") {
+      for (let shade in theme("colors")[key]) {
+        const bgColor = theme("colors")[key][shade];
+        const textColor = lightOrDarkColor(bgColor) === "dark" ? "white" : "black";
+
+        components[`.btn-${key}-${shade}`] = {
+          ...components[".btn"],
+          backgroundColor: bgColor,
+          color: textColor,
+        };
       }
+    } else {
+
+      const textColor = lightOrDarkColor(theme("colors")[key]) === "dark" ? "white" : "black";
+
+      components[`.btn-${key}`] = {
+        ...components[".btn"],
+        backgroundColor: theme("colors")[key],
+        color: textColor,
+      };
     }
   }
 
-  matchComponents({
+  addComponents(components);
+
+  matchUtilities({
     btn: (value) => {
       return {
+        ...components[".btn"],
         backgroundColor: value,
         color: lightOrDarkColor(value) === "dark" ? "white" : "black",
-      }
-    }
-  })
+      };
+    },
+  });
 });
 
 module.exports = buttonPlugin;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,js}"],
+  safelist: [{ pattern: /^btn-/ }],
   theme: {
     extend: {
       keyframes: {
